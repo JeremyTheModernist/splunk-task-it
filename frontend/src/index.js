1e277b8626eb9d6d89dea485ea42f585489b58dc
Jeremy Barnes <jeremyb1988@gmail.com>
2021-02-22 12:16:38 -0500

    update:minor changes.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter as Router } from "react-router-dom";
 import { PageWrapper, TodoWrapper } from "./state";
 
-// LogRocket.init("zc2cw6/my-todo-app-dev");
+LogRocket.init("jeremys-org/splunk-todo-app");
 
 ReactDOM.render(
   <React.StrictMode>
